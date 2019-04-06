27d8ed285db16ebcfcb9f519616da48801aeaa37
MXWXZ <me@imwxz.com>
2019-04-06 22:42:21 +0800

    2.0 initial

--- src/index.js
+++ src/index.js
@@ -2,22 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Main from './main';
-import Signup from './signup';
-import Cart from './cart';
-import Explore from './explore';
-import Book1 from './book1';
-import Book2 from './book2';
-import Book3 from './book3';
+import "antd/dist/antd.css";
+// import Signup from './signup';
+// import Cart from './cart';
+// import Explore from './explore';
+// import Book1 from './book1';
+// import Book2 from './book2';
+// import Book3 from './book3';
 
 const StoreRouter = (
     <Router>
         <Route exact path="/" component={Main} />
-        <Route path="/explore" component={Explore} />
+        {/* <Route path="/explore" component={Explore} />
         <Route path="/book1" component={Book1} />
         <Route path="/book2" component={Book2} />
         <Route path="/book3" component={Book3} />
         <Route path="/Cart" component={Cart} />
-        <Route path="/signup" component={Signup} />
+        <Route path="/signup" component={Signup} /> */}
     </Router>
 );
 
--- src/main.js
+++ src/main.js
@@ -1,41 +1,18 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import Header from './component/Header';
-require('./css/main.css');
+import { Layout } from 'antd';
+import Header from './components/Header';
+
 
 class Main extends Component {
     render() {
+        const nav = [
+            { name: 'Home', url: '/' },
+            { name: 'Explore', url: '/explore' }
+        ];
         return (
-            <div>
-                <Header current='0' />
-
-                <div className="recom" style={{ background: 'url(' + require('./img/bg.gif') + ') no-repeat center top' }}>
-                    <div className="recom_wrap">
-                        <div className="title">
-                            <h2>
-                                <Link to="/book1" target="_blank">
-                                    <i>The Man Who Changed China</i>
-                                </Link>
-                            </h2>
-                            <p>You should sacrifice 1s for the elder...</p>
-                        </div>
-                        <div className="book">
-                            <div className="book_wrap">
-                                <ul>
-                                    <li>
-                                        <Link to="/book1" target="_blank">
-                                            <img src={require('./img/book1.jpg')} alt="The Man Who Changed China" />
-                                        </Link>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="waiting">
-                            <h2>Coming Soon, be patient!</h2>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Layout className="layout">
+                <Header nav={nav} />
+            </Layout>
         );
     }
 }
